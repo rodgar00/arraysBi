17aa7fb4d2d852d252b170e639c167de1c328538
rodgar00 <rodrigogarciaperez00@gmail.com>
2025-02-19 09:43:43 +0100

    Fin

--- ej9.py
+++ ej9.py
@@ -1,3 +1,16 @@
-filas = 20
-columas = 2
-matriz = []
+from ayuda import printMatriz
+
+tabla = []
+
+for filas in range(20):
+    fila = []
+    for columnas in range(2):
+        if columnas == 0:
+            exponente = int(input(f"Ingrese el exponente {filas + 1}/20: "))
+            fila.append(exponente)
+        else:
+            fila.append(2 ** exponente)
+    tabla.append(fila)
+
+print("\nTabla de potencias de 2:")
+printMatriz(tabla)
